ab249f5225b75f442b123569da043d61d7d9695c
Arpan Sahoo <asahoojhu@gmail.com>
2021-05-17 00:12:22 -0400

    Created json of gene sets

--- backend/go/make_genesets.py
+++ backend/go/make_genesets.py
@@ -4,7 +4,7 @@ import json
 from progress.bar import Bar
 
 def main():
-  go_basic = pronto.Ontology("http://purl.obolibrary.org/obo/go/go-basic.obo")
+  go_basic = pronto.Ontology.from_obo_library("go/go-basic.obo")
   gene_sets = dict()
 
   bar = Bar('Processing', max=len(go_basic))
@@ -15,8 +15,11 @@ def main():
     if type(term) == pronto.term.Term:
       req = requests.get("http://api.geneontology.org/api/bioentity/function/" + term.id + "/genes?facet=false&unselect_evidence=true&exclude_automatic_assertions=true&fetch_objects=false&use_compact_associations=false&relationship_type=involved_in")
       json_data = json.loads(req.text)
-
       curr_set = set()
+
+      if "associations" not in json_data:
+        continue
+
       for entry in json_data["associations"]:
         if entry["object"]["id"] == term.id:
           gene_name = entry["subject"]["label"]
